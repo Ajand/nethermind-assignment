4d134594ace82b887b05cdb5e1e60b7d1321a868
Ajand Mardalizad <aseedgeek@gmail.com>
2022-08-10 22:29:38 +0400

    home template added

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -16,6 +16,26 @@ const Home: NextPage = () => {
 
       <main className="main">
         <Navbar />
+        <div className="content">
+          <div className="panel p-10 ">
+            <p className="text-3xl text-white">
+              Enter any 256-bit hexadecimal value to find a nonce that generates
+              a keccak256 hash that is less than your input.
+            </p>
+            <input
+              className="w-full mt-6 px-6 p-4 text-2xl input-background text-white"
+              placeholder="What is your input?"
+            />
+            <p className="text-lg mt-2 text-gray-300">
+              Only 256-bit hexadecimal is allowed.
+            </p>
+            <div className="flex justify-center">
+              <button className="btn btn-red p-4 mt-4 text-center">
+                Find The Nonce
+              </button>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
