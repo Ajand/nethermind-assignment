2b2ee69b8d9667d4a7b140d1679121fb2cda2115
Ajand Mardalizad <aseedgeek@gmail.com>
2022-08-10 21:01:45 +0400

    navbar template added

---
+++ client/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="text-white flex text-3xl py-8 px-16">
+      <div className="cursor-pointer font-extrabold	">Home</div>
+      <div className="ml-8 cursor-pointer">Que</div>
+      <div className="ml-8 cursor-pointer">Workers</div>
+    </div>
+  );
+};
+
+export default Navbar;
