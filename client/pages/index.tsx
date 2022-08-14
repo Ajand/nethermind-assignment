15b0518b9435fc7a1d5dc4c7e69bd3b203f4bc57
Ajand Mardalizad <aseedgeek@gmail.com>
2022-08-14 15:54:16 +0430

    apollo client added

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,30 @@
 import type { NextPage } from "next";
-
+import { useState } from "react";
+import { ethers } from "ethers";
 import Navbar from "../components/Navbar";
+import { gql, useMutation } from "@apollo/client";
 
 const Home: NextPage = () => {
+  const [input, setInput] = useState("");
+
+  const ADD_TO_QUEUE = gql`
+    mutation AddToQueue($value: String!) {
+      addToQueue(value: $value)
+    }
+  `;
+
+  const [addToQueue] = useMutation(ADD_TO_QUEUE);
+
+  const isValid = () => {
+    if (input.length !== 64) return false;
+    try {
+      const hexa = ethers.BigNumber.from("0x" + input);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  };
+
   return (
     <div>
       <main className="main">
@@ -16,12 +38,32 @@ const Home: NextPage = () => {
             <input
               className="w-full mt-6 px-6 p-4 text-2xl input-background text-white"
               placeholder="What is your input?"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
             />
-            <p className="text-lg mt-3 text-gray-300">
-              Only 256-bit hexadecimal is allowed.
-            </p>
+            {!isValid() && (
+              <p className="text-lg mt-3 text-gray-300">
+                Only 256-bit hexadecimal is allowed.
+              </p>
+            )}
+
             <div className="flex justify-center">
-              <button className="btn btn-red p-4 mt-4 text-center">
+              <button
+                className={`${
+                  isValid() ? "btn btn-red" : "btn-disabled"
+                } p-4 mt-4 text-center`}
+                onClick={() => {
+                  if (isValid()) {
+                    addToQueue({ variables: { value: input } })
+                      .then((r) => {
+                        console.log(r);
+                      })
+                      .catch((err) => {
+                        console.log(err);
+                      });
+                  }
+                }}
+              >
                 Find The Nonce
               </button>
             </div>
